20a9588de28c59da3032865d909f540bbdcf980f
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-27 11:02:41 +0530

    [Abhishek Jat] Add. Setting up all necessary dependencies for employee payroll app

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeePayrollApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // This run the EmployeePayRollApplication
+        SpringApplication.run(EmployeePayrollApplication.class, args);
+    }
 }
